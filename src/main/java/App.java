02637d88715652ab3a9e01d489fcb4453c239b3e
Adam Wang <hwang12wou@gmail.com>
2017-04-04 10:55:24 -0400

    java classes created

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,51 +18,57 @@ import com.test.rss.*;
 
 public class App {
 	public static void main(String[] args) {
-		ObjectFactory factory = new ObjectFactory();
-		try {
-			File file = new File("src/main/xml/file.xml");
-			
-			Employee employee1 = factory.createEmployee();
-			employee1.setEmplyeeId(1);
-			employee1.setClient("Client1");
-			employee1.setEmployeeName("France");
-			employee1.setProject("Project1");
-			
-			Employee employee2 = factory.createEmployee();
-			employee2.setEmplyeeId(2);
-			employee2.setClient("Client2");
-			employee2.setEmployeeName("Brown");
-			employee2.setProject("Project2");
-
-			Employer employer = factory.createEmployer();
-			employer.setEmployerId(1);
-			employer.setEmployerName("Damon");
-			
-
-
-			Department department = factory.createDepartment();
-			department.setDepartmentId(1);
-			department.setDepartmentName("Google");
-			department.setEmployer(employer);
-			
-			List<Employee> empList =  department.getEmployee();
-			empList.add(employee1);
-			empList.add(employee2);
-
-			JAXBContext context = JAXBContext.newInstance(ObjectFactory.class);
-			JAXBElement<Department> element = factory.createNewDepartment(department);
-
-			Marshaller marshaller = context.createMarshaller();
-			marshaller.setProperty("jaxb.formatted.output", Boolean.TRUE);
-			marshaller.marshal(element, System.out);
-			marshaller.marshal(element, file);
-			boolean valid = validateXMLSchema("src/main/xsd/complex.xsd", "src/main/xml/file.xml");
-			System.out.println();
-			System.out.println("========Validation result========");
-			System.out.println(valid);
-		} catch (JAXBException e) {
-			e.printStackTrace();
-		}
+//		ObjectFactory factory = new ObjectFactory();
+//		try {
+//			File file = new File("src/main/xml/file.xml");
+//			
+//			Employee employee1 = factory.createEmployee();
+//			List<String> e1Project = employee1.getProject();
+//			employee1.setEmplyeeId(1);
+//			employee1.setClient("Client1");
+//			employee1.setEmployeeName("France");
+//			e1Project.add("e1Project1");
+//			e1Project.add("e1Project2");
+//			
+//			Employee employee2 = factory.createEmployee();
+//			List<String> e2Project = employee2.getProject();
+//			employee2.setEmplyeeId(2);
+//			employee2.setClient("Client2");
+//			employee2.setEmployeeName("Brown");
+//			e2Project.add("e2Project1");
+//			e2Project.add("e2Project2");
+//			e2Project.add("e2Project3");
+//			e2Project.add("e2Project4");
+//
+//			Employer employer = factory.createEmployer();
+//			employer.setEmployerId(1);
+//			employer.setEmployerName("Damon");
+//			
+//
+//
+//			Department department = factory.createDepartment();
+//			department.setDepartmentId(1);
+//			department.setDepartmentName("Google");
+//			department.setEmployer(employer);
+//			
+//			List<Employee> empList =  department.getEmployee();
+//			empList.add(employee1);
+//			empList.add(employee2);
+//
+//			JAXBContext context = JAXBContext.newInstance(ObjectFactory.class);
+//			JAXBElement<Department> element = factory.createNewDepartment(department);
+//
+//			Marshaller marshaller = context.createMarshaller();
+//			marshaller.setProperty("jaxb.formatted.output", Boolean.TRUE);
+//			marshaller.marshal(element, System.out);
+//			marshaller.marshal(element, file);
+//			boolean valid = validateXMLSchema("src/main/xsd/complex.xsd", "src/main/xml/file.xml");
+//			System.out.println();
+//			System.out.println("========Validation result========");
+//			System.out.println(valid);
+//		} catch (JAXBException e) {
+//			e.printStackTrace();
+//		}
 
 	}
 
